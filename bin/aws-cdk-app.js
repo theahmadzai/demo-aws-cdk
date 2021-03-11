2d566528a1f7e879a8743f8aadd3cc67d7730d89
Muhammad Javed <theahmadzai@hotmail.com>
2021-03-11 12:44:41 +0500

    Add files

--- bin/aws-cdk-app.js
+++ bin/aws-cdk-app.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
-const { AwsCdkAppStack } = require('../lib/aws-cdk-app-stack');
+const cdk = require('@aws-cdk/core')
+const { AwsCdkAppStack } = require('../lib/aws-cdk-app-stack')
 
-const app = new cdk.App();
-new AwsCdkAppStack(app, 'AwsCdkAppStack');
+const app = new cdk.App()
+new AwsCdkAppStack(app, 'AwsCdkAppStack', {
+  env: {
+    account: '674122170248',
+    region: 'us-east-1',
+  },
+})
