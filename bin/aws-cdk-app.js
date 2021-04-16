278bcaadbf52aeaeed1ea32caf60cc5468d3e9b7
Muhammad Javed <theahmadzai@hotmail.com>
2021-04-16 12:20:47 +0500

    Add GraphQL Stack

--- bin/aws-cdk-app.js
+++ bin/aws-cdk-app.js
@@ -2,6 +2,8 @@
 
 const cdk = require('@aws-cdk/core')
 const { AwsCdkAppStack } = require('../lib/aws-cdk-app-stack')
+const { AwsCdkGraphqlStack } = require('../lib/aws-cdk-graphql-stack')
 
 const app = new cdk.App()
 new AwsCdkAppStack(app, 'AwsCdkAppStack')
+new AwsCdkGraphqlStack(app, 'AwsCdkGraphqlStack')
