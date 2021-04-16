473ce8216ee78b51f490089d57b7dac7e07f832a
Muhammad Javed <theahmadzai@hotmail.com>
2021-04-16 12:26:32 +0500

    Handle multiple fieldNames in lambda datasource

--- lambda/graphql-api.js
+++ lambda/graphql-api.js
@@ -1,3 +1,12 @@
-exports.handler = async () => {
-  return 'yay...'
+const resolvers = {
+  hello: () => 'world',
+  world: () => 'hello',
+}
+
+exports.handler = async (event) => {
+  if (!Object.keys(resolvers).includes(event.info.fieldName)) {
+    return null
+  }
+
+  return resolvers[event.info.fieldName]()
 }
